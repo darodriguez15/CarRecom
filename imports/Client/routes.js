0ef6ef2ea6329eda6eab51784f61aba6f60d8157
Diego Andrés Rodriguez <da.rodriguez15@uniandes.edu.co>
2017-12-09 17:26:55 -0500

    Fix

--- imports/Client/routes.js
+++ imports/Client/routes.js
@@ -8,14 +8,13 @@ const browserHistory = createBrowserHistory();
 import mainPage from "./Components/mainPage";
 import imagenes from "./Components/items";
 import Brands from "./Components/Brands/Index";
-import Prueba from "./Components/Brands/Brands";
+
 
 export const renderRoutes = () => (
 
     <Router history={browserHistory}>
     <div>
     <Route exact path="/" component={mainPage}/>
-    <Route path="/prueba" component = {Prueba}/>
     <Route path="/imagenes" component = {imagenes}/>
     <Route path="/brands" component = {Brands}/>
 </div>
